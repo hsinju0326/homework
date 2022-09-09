b6d2b28ca196f470202dfb9c52ac1710e0b4a078
hsinju <kate880326@yahoo.com.tw>
2022-09-09 15:33:44 +0800

    Java and Kotlin ticket HW2

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -4,12 +4,19 @@ import java.util.Scanner;
 
 public class Tester {
     public static void main(String[] args) {
-        System.out.print("Please enter number of tickets: ");
-        Scanner scanner = new Scanner(System.in);
-        int ticket = scanner.nextInt();
-        System.out.print("How many round-trip tickets:  ");
-        int RTticket = scanner.nextInt();
-        Fare far = new Fare(ticket, RTticket);
-        far.print();
+        int ticket = 0;
+        while (ticket != -1) {
+            System.out.print("Please enter number of tickets: ");
+            Scanner scanner = new Scanner(System.in);
+            ticket = scanner.nextInt();
+            if (ticket != -1) {
+                System.out.print("How many round-trip tickets:  ");
+                int RTticket = scanner.nextInt();
+                Fare far = new Fare(ticket, RTticket);
+                far.print();
+            } else {
+                break;
+            }
+         }
     }
 }
--- src/main/kotlin/com/train/TesterKotlin.kt
+++ src/main/kotlin/com/train/TesterKotlin.kt
@@ -3,13 +3,20 @@ package com.train
 import java.util.*
 
 fun main(args: Array<String>) {
-    val scanner = Scanner(System.`in`)
-    print("Please enter number of tickets: ")
-    var ticket = scanner.nextInt()
-    print("How many round-trip tickets:  ")
-    var RTticket = scanner.nextInt()
-    val tic = Ticket(ticket, RTticket)
-    tic.print()
+    val ticket = 0
+    while (ticket != -1) {
+        val scanner = Scanner(System.`in`)
+        print("Please enter number of tickets: ")
+        var ticket = scanner.nextInt()
+        if (ticket != -1) {
+            print("How many round-trip tickets:  ")
+            var RTticket = scanner.nextInt()
+            val tic = Ticket(ticket, RTticket)
+            tic.print()
+        } else {
+            break
+        }
+    }
 }
 
 class Ticket(var ticket: Int, var RTticket: Int) {
